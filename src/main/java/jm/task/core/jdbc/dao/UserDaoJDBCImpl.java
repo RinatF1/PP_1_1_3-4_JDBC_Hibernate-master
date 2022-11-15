80d12e44473285d22a05e3f4ea70c06684a3a341
RinatF1 <RinatF1@yandex.ru>
2022-11-15 14:28:06 +0500

    private final Connection con

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
 
-Connection con = Util.getConnection();
+private final Connection con = Util.getConnection();
 
  public UserDaoJDBCImpl() {
 
